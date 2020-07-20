683414237e0cee58b91af5c28a7ae7e0a9232c62
Mikael Brudfors <brudfors@gmail.com>
2020-07-20 16:38:00 +0100

    Updated demo figure

--- CTseg.m
+++ CTseg.m
@@ -6,8 +6,8 @@ function seg = CTseg(in, odir, tc, def, correct_header)
 %     1. Gray matter (GM)
 %     2. White matter (WM)
 %     3. Cerebrospinal fluid (CSF)
-%     4. Dural venous sinuses, calcifications and hyper-intensities (SIN)
-%     5. Skull (BONE)
+%     4. Dural venous sinuses (SIN)
+%     5. Skull, calcifications and hyper-intensities (BONE)
 %     6. Soft tissue (ST)
 %     7. Background (BG),
 % the outputs are prefixed as the SPM12 unified segmentation (c*, wc*, mwc*).
